afb308d88e08f85db382d3c2969056dc75bf3d3a
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-19 20:28:50 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brayan Duwe on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
