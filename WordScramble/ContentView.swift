1e3aead8545a731a47d88e90e2ae7ec92ed32c03
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-19 21:15:48 -0300

    first commit

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -17,6 +17,15 @@ struct ContentView: View {
         }
         .padding()
     }
+    func testString() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
 }
 
 #Preview {
